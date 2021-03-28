1dc9d76390d7d4a345c9ac1e8b94a011a466cb2c
Hugi Thordarson <hugi@karlmenn.is>
2021-03-28 15:07:37 +0000

    Add more missing constants

--- ERExtensions/src/main/java/er/extensions/eof/ERXConstant.java
+++ ERExtensions/src/main/java/er/extensions/eof/ERXConstant.java
@@ -9,4 +9,5 @@ public class ERXConstant {
 	public static final Class[] NotificationClassArray = { com.webobjects.foundation.NSNotification.class };
     public static final Integer OneInteger = 1;
     public static final Integer ZeroInteger = 0;
+    public static final Class[] StringClassArray = new Class[] { String.class };
 }
