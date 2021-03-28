780f84babfd257316af7f9084b8888d444caa501
Hugi Thordarson <hugi@karlmenn.is>
2021-03-28 17:17:06 +0000

    Deleted ERXKeyValuePair

--- ERExtensions/src/main/java/er/extensions/foundation/ERXKeyValueCodingUtilities.java
+++ ERExtensions/src/main/java/er/extensions/foundation/ERXKeyValueCodingUtilities.java
@@ -3,7 +3,6 @@ package er.extensions.foundation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.util.Collection;
 import java.util.Hashtable;
 import java.util.Iterator;
@@ -106,38 +105,6 @@ public class ERXKeyValueCodingUtilities {
         }
         return result;
     }
-
-    /**
-     * Returns final strings constants from an interface or class. Useful in particular when you want to create
-     * selection lists from your interfaces automatically. 
-     * @param c
-     * @return list of final string constants
-     */
-    public static NSArray<ERXKeyValuePair> staticStringsForClass(Class c) {
-		NSMutableArray<ERXKeyValuePair> result = new NSMutableArray();
-		if(c.getSuperclass() != null) {
-			result.addObjectsFromArray(staticStringsForClass(c.getSuperclass()));
-		}
-		Field[] fields = c.getDeclaredFields();
-		for (int i = 0; i < fields.length; i++) {
-			Field field = fields[i];
-			try {
-				if(Modifier.isFinal(field.getModifiers()) && field.getType().equals(String.class)) {
-					String key = field.getName();
-					String value = (String) field.get(c);
-					result.addObject(new ERXKeyValuePair(key, value));
-				}
-			}
-			catch (IllegalArgumentException e) {
-				throw NSForwardException._runtimeExceptionForThrowable(e);
-			}
-			catch (IllegalAccessException e) {
-				throw NSForwardException._runtimeExceptionForThrowable(e);
-			}
-		}
-		return result;
-
-	}
 
     public static final NSKeyValueCodingAdditions Statics = new NSKeyValueCodingAdditions() {
         /**
