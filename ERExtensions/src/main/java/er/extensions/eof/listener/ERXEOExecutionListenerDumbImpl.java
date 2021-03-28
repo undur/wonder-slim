7c6ef6127ef2f1dac084439ecc5a82b821018960
Hugi Thordarson <hugi@karlmenn.is>
2021-03-28 16:55:17 +0000

    Deleted package er.extensions.eof.listener

--- ERExtensions/src/main/java/er/extensions/eof/listener/ERXEOExecutionListenerDumbImpl.java
+++
@@ -1,7 +0,0 @@
-package er.extensions.eof.listener;
-
-public class ERXEOExecutionListenerDumbImpl implements IERXEOExecutionListener {
-    public void log(long requestTime, String entityName) {
-        //Do nothing here
-    }
-}
--- ERExtensions/src/main/java/er/extensions/eof/listener/IERXEOExecutionListener.java
+++
@@ -1,7 +0,0 @@
-package er.extensions.eof.listener;
-
-public interface IERXEOExecutionListener {
-
-    public void log(long requestTime, String entityName);
-
-}
