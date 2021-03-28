5695c3ab03db655084fdf9d555ff47ab2db44ac0
Hugi Thordarson <hugi@karlmenn.is>
2021-03-28 14:23:06 +0000

    Added FIXMEException. Kill me.

---
+++ ERExtensions/src/main/java/er/extensions/FIXMEException.java
@@ -0,0 +1,15 @@
+package er.extensions;
+
+/**
+ * Temporary exception class added to mark the places that need fixup in the
+ * Wonder/Undur migration process.
+ * 
+ * I should not exist, but here I am.
+ */
+
+public class FIXMEException extends RuntimeException {
+
+	public FIXMEException(String string) {
+		super(string);
+	}
+}
