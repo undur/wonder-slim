94d80cd2ac16debb184321eb4c19ef454a2416ee
Hugi Thordarson <hugi@karlmenn.is>
2021-03-28 15:48:20 +0000

    Add more stupid constants

--- ERExtensions/src/main/java/er/extensions/eof/ERXConstant.java
+++ ERExtensions/src/main/java/er/extensions/eof/ERXConstant.java
@@ -1,7 +1,9 @@
 package er.extensions.eof;
 
 /**
- * FIXME: Introduced temporarily to serve as container for the public variables used from here. 
+ * FIXME: Everything here is stupid.
+ * 
+ * This class was introduced temporarily to serve as container for the public variables previously used from here, until they get replaced on-site. 
  */
 
 public class ERXConstant {
@@ -10,4 +12,20 @@ public class ERXConstant {
     public static final Integer OneInteger = 1;
     public static final Integer ZeroInteger = 0;
     public static final Class[] StringClassArray = new Class[] { String.class };
+
+    public static final int MAX_INT=2500;
+
+    protected static Integer[] INTEGERS=new Integer[MAX_INT];
+
+    static {
+        for (int i=0; i<MAX_INT; i++) INTEGERS[i]=Integer.valueOf(i);
+    }
+
+    /**
+     * Returns an Integer for a given int
+     * @return potentially cache Integer for a given int
+     */
+    public static Integer integerForInt(int i) {
+        return (i>=0 && i<MAX_INT) ? INTEGERS[i] : Integer.valueOf(i);
+    }
 }
