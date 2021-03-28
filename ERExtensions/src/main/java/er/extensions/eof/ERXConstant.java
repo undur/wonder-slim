2df47e5c72f4f74ba29cc205e1194c8d1a08d2b2
Hugi Thordarson <hugi@karlmenn.is>
2021-03-28 16:52:58 +0000

    I have no idea

--- ERExtensions/src/main/java/er/extensions/eof/ERXConstant.java
+++ ERExtensions/src/main/java/er/extensions/eof/ERXConstant.java
@@ -1,5 +1,14 @@
 package er.extensions.eof;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.webobjects.foundation.NSArray;
+import com.webobjects.foundation.NSMutableArray;
+
+import er.extensions.foundation.ERXArrayUtilities;
+
 /**
  * FIXME: Everything here is stupid.
  * 
@@ -31,4 +40,49 @@ public class ERXConstant {
     public static Integer integerForInt(int i) {
         return (i>=0 && i<MAX_INT) ? INTEGERS[i] : Integer.valueOf(i);
     }
+    
+    public static interface Constant {
+		public int sortOrder();
+		public String name();
+		public Object value();
+	}
+    
+	/**
+	 * Holds the value store, grouped by class name.
+	 */
+	private static final Map _store = new HashMap();
+	
+    /**
+     * Retrieves the constant for the given class name and value. Null is returned
+     * if either class or value isn't found.
+     * @param value
+     * @param clazzName
+     */
+    public static Constant constantForClassNamed(Object value, String clazzName) {
+        synchronized (_store) {
+            Map classMap = keyMap(clazzName, false);
+            Constant result = (Constant) classMap.get(value);
+            return result;
+        }
+    }
+
+	/**
+	 * Retrieves the key map for the class name.
+	 * @param name
+	 * @param create
+	 */
+	private static Map keyMap(String name, boolean create) {
+		Map map = (Map) _store.get(name);
+		if(map == null) {
+			if(create) {
+				map = new HashMap();
+				_store.put(name, map);
+				name = name.replace('$', '.');
+				_store.put(name, map);
+			} else {
+				map = Collections.EMPTY_MAP;
+			}
+		}
+		return map;
+	}
 }
