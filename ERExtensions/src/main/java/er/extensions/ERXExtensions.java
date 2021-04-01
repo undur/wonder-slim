c760b3fb573a26e3b913ed9b532dad7c2c346615
Hugi Thordarson <hugi@karlmenn.is>
2021-04-01 21:09:43 +0000

    Standardized formatting

--- ERExtensions/src/main/java/er/extensions/ERXExtensions.java
+++ ERExtensions/src/main/java/er/extensions/ERXExtensions.java
@@ -56,183 +56,203 @@ import er.extensions.validation.ERXValidationFactory;
 import x.FIXMEException;
 
 /**
- * Principal class of the ERExtensions framework. This class
- * will be loaded at runtime when the ERExtensions bundle is
- * loaded (even before the Application constructor is called)
- * This class has a boat-load of stuff in it that will hopefully
- * be finding better homes in the future. This class serves as
- * the initialization point of this framework, look in the static
- * initializer to see all the stuff that is initially setup when
- * this class is loaded. This class also has a boat load of
- * string, array and EOF utilities as well as the factory methods
- * for creating editing contexts with the default delegates set.
+ * Principal class of the ERExtensions framework. This class will be loaded at
+ * runtime when the ERExtensions bundle is loaded (even before the Application
+ * constructor is called) This class has a boat-load of stuff in it that will
+ * hopefully be finding better homes in the future. This class serves as the
+ * initialization point of this framework, look in the static initializer to see
+ * all the stuff that is initially setup when this class is loaded. This class
+ * also has a boat load of string, array and EOF utilities as well as the
+ * factory methods for creating editing contexts with the default delegates set.
  */
 public class ERXExtensions extends ERXFrameworkPrincipal {
 
-    private static boolean _initialized;
+	private static boolean _initialized;
 
-   /**
-     * Configures the framework. All the bits and pieces that need
-     * to be configured are configured, those that need to happen
-     * later are delayed by registering an observer for notifications
-     * that are posted when the application is finished launching.
-     * This public observer is used to perform basic functions in
-     * response to notifications. Specifically it handles
-     * configuring the adaptor context so that SQL debugging can
-     * be enabled and disabled on the fly through the log4j system.
-     * Handling cleanup issues when sessions timeout, i.e. releasing
-     * all references to editing contexts created for that session.
-     * Handling call all of the <code>did*</code> methods on
-     * {@link ERXGenericRecord} subclasses after an editing context
-     * has been saved. This delegate is also responsible for configuring
-     * {@link ERXValidationFactory}.
-     * This delegate is configured when this framework is loaded.
-     */
-    @Override
-    protected void initialize() {
-    	NSNotificationCenter.defaultCenter().addObserver(this,
-    			new NSSelector("bundleDidLoad", ERXConstant.NotificationClassArray),
-    			ERXApplication.AllBundlesLoadedNotification,
-    			null);
-    }
+	/**
+	 * Configures the framework. All the bits and pieces that need to be
+	 * configured are configured, those that need to happen later are delayed by
+	 * registering an observer for notifications that are posted when the
+	 * application is finished launching. This public observer is used to
+	 * perform basic functions in response to notifications. Specifically it
+	 * handles configuring the adaptor context so that SQL debugging can be
+	 * enabled and disabled on the fly through the log4j system. Handling
+	 * cleanup issues when sessions timeout, i.e. releasing all references to
+	 * editing contexts created for that session. Handling call all of the
+	 * <code>did*</code> methods on {@link ERXGenericRecord} subclasses after an
+	 * editing context has been saved. This delegate is also responsible for
+	 * configuring {@link ERXValidationFactory}. This delegate is configured
+	 * when this framework is loaded.
+	 */
+	@Override
+	protected void initialize() {
+		NSNotificationCenter.defaultCenter().addObserver(this,
+				new NSSelector("bundleDidLoad", ERXConstant.NotificationClassArray),
+				ERXApplication.AllBundlesLoadedNotification,
+				null);
+	}
 
-    public void bundleDidLoad(NSNotification n) {
-    	if(_initialized) return;
-    	_initialized = true;
-    	
-    	try {
-    		// This will load any optional configuration files, 
-    		// ensures that WOOutputPath's was processed with this @@
-    		// variable substitution. WOApplication uses WOOutputPath in
-    		// its constructor so we need to modify it before calling
-    		// the constructor.
-    		ERXConfigurationManager.defaultManager().initialize();
-        	ERXSystem.updateProperties();
- 
-    		// AK: enable this when we're ready
-        	// WOEncodingDetector.sharedInstance().setFallbackEncoding(CharEncoding.UTF_8);
-        	
-        	// GN: configure logging with optional custom subclass of ERXLogger
-        	String className = ERXProperties.stringForKey("er.extensions.erxloggerclass"); 
-        	if (className != null) {
-	        	Class loggerClass = Class.forName(className);
-	        	Method method = loggerClass.getDeclaredMethod(ERXLogger.CONFIGURE_LOGGING_WITH_SYSTEM_PROPERTIES, (Class[]) null);
-	        	method.invoke(loggerClass, (Object[]) null);
-        	}
-        	else {
-        		// default behaviour:
-        		ERXLogger.configureLoggingWithSystemProperties();
-        	}
-        	
-            ERXArrayUtilities.initialize();
-    	} catch (Exception e) {
-    		throw NSForwardException._runtimeExceptionForThrowable(e);
-    	}
-    }
+	public void bundleDidLoad(NSNotification n) {
 
-    /**
-     * This method is called when the application has finished
-     * launching. Here is where log4j is configured for rapid
-     * turn around and the validation template system is configured.
-     */
-    @Override
-    public void finishInitialization() {
-        // AK: we now setup the properties three times. At startup, in ERX.init
+		if (_initialized) {
+			return;
+		}
+
+		_initialized = true;
+
+		try {
+			// This will load any optional configuration files,
+			// ensures that WOOutputPath's was processed with this @@
+			// variable substitution. WOApplication uses WOOutputPath in
+			// its constructor so we need to modify it before calling
+			// the constructor.
+			ERXConfigurationManager.defaultManager().initialize();
+			ERXSystem.updateProperties();
+
+			// AK: enable this when we're ready
+			// WOEncodingDetector.sharedInstance().setFallbackEncoding(CharEncoding.UTF_8);
+
+			// GN: configure logging with optional custom subclass of ERXLogger
+			String className = ERXProperties.stringForKey("er.extensions.erxloggerclass");
+			if (className != null) {
+				Class loggerClass = Class.forName(className);
+				Method method = loggerClass.getDeclaredMethod(ERXLogger.CONFIGURE_LOGGING_WITH_SYSTEM_PROPERTIES, (Class[]) null);
+				method.invoke(loggerClass, (Object[]) null);
+			}
+			else {
+				// default behaviour:
+				ERXLogger.configureLoggingWithSystemProperties();
+			}
+
+			ERXArrayUtilities.initialize();
+		}
+		catch (Exception e) {
+			throw NSForwardException._runtimeExceptionForThrowable(e);
+		}
+	}
+
+	/**
+	 * This method is called when the application has finished launching. Here
+	 * is where log4j is configured for rapid turn around and the validation
+	 * template system is configured.
+	 */
+	@Override
+	public void finishInitialization() {
+		// AK: we now setup the properties three times. At startup, in ERX.init
 		// and here. Note that this sucks beyond belief, as this will produce
 		// unforeseen results in several cases, but it's the only way to set up
-		// all parts of the property handling. The first install only loads plain
-		// and user props the second has no good way to set up the main bundle and this one
+		// all parts of the property handling. The first install only loads
+		// plain
+		// and user props the second has no good way to set up the main bundle
+		// and this one
 		// comes too late for static inits
-    	ERXConfigurationManager.defaultManager().loadConfiguration();
-    	
-        ERXProperties.populateSystemProperties();
-        
-        ERXConfigurationManager.defaultManager().configureRapidTurnAround();
-        ERXLocalizer.initialize();
-        ERXValidationFactory.defaultFactory().configureFactory();
-        // update configuration with system properties that might depend
-        // on others like 
-        // log4j.appender.SQL.File=@@loggingBasePath@@/@@port@@.sql
-        // loggingBasePath=/var/log/@@name@@
-        // name and port are resolved via WOApplication.application()
-        // ERXLogger.configureLoggingWithSystemProperties();
-        
-		ERXProperties.pathsForUserAndBundleProperties(true);
-    }
-    
-    /**
-     * Initializes your WOApplication programmatically (for use in test cases and main methods) with
-     * the assumption that the current directory is your main bundle URL.
-     * 
-     * @param applicationSubclass your Application subclass
-     * @param args the commandline arguments for your application
-     */
-    public static void initApp(Class applicationSubclass, String[] args) {
-		try {
-	    	File woaFolder = new File(".").getCanonicalFile();
-	    	if (!woaFolder.getName().endsWith(".woa")) {
-	    		if (new File(woaFolder, ".project").exists()) {
-	    			File buildFolder = new File(new File(woaFolder, "build"), woaFolder.getName() + ".woa");
-	    			if (buildFolder.exists()) {
-	    				woaFolder = buildFolder;
-	    			}
-	    			else {
-		    			File distFolder = new File(new File(woaFolder, "dist"), woaFolder.getName() + ".woa");
-	    				if (distFolder.exists()) {
-	    					woaFolder = distFolder;
-	    				}
-	    				else {
-	    					//Bundle-less builds. Yay!
-	    		    		//throw new IllegalArgumentException("You must run your application from a .woa folder to call this method.");
-	    				}
-	    			}
-	    		}
-	    	}
-	    	ERXExtensions.initApp(null, woaFolder.toURI().toURL(), applicationSubclass, args);
-		}
-		catch (IOException e) {
-			throw new NSForwardException(e);
-		}
-    }
-    
-    /**
-     * Initializes your WOApplication programmatically (for use in test cases and main methods).
-     * 
-     * @param mainBundleName the name of your main bundle
-     * @param applicationSubclass your Application subclass
-     * @param args the commandline arguments for your application
-     */
-    public static void initApp(String mainBundleName, Class applicationSubclass, String[] args) {
-    	ERXExtensions.initApp(mainBundleName, null, applicationSubclass, args);
-    }
-    
-    private static boolean _appInitialized = false;
+		ERXConfigurationManager.defaultManager().loadConfiguration();
 
-    /**
-     * Initializes your WOApplication programmatically (for use in test cases and main methods).
-     * 
-     * @param mainBundleName the name of your main bundle (or null to use mainBundleURL)
-     * @param mainBundleURL the URL to your main bundle (ignored if mainBundleName is set)
-     * @param applicationSubclass your Application subclass
-     * @param args the commandline arguments for your application
-     */
-    public static void initApp(String mainBundleName, URL mainBundleURL, Class applicationSubclass, String[] args) {
-    	if (_appInitialized) {
-    		return;
-    	}
-    	try {
-	        ERXApplication.setup(args);
-	        if (mainBundleURL != null) {
-		        System.setProperty("webobjects.user.dir", new File(mainBundleURL.getFile()).getCanonicalPath());
-	        }
-	        // Odds are you are only using this method for test cases and development mode
-	        System.setProperty("er.extensions.ERXApplication.developmentMode", "true");
-	        ERXApplication.primeApplication(mainBundleName, mainBundleURL, applicationSubclass.getName());
-	        //NSNotificationCenter.defaultCenter().postNotification(new NSNotification(ERXApplication.ApplicationDidCreateNotification, WOApplication.application()));
+		ERXProperties.populateSystemProperties();
+
+		ERXConfigurationManager.defaultManager().configureRapidTurnAround();
+		ERXLocalizer.initialize();
+		ERXValidationFactory.defaultFactory().configureFactory();
+		// update configuration with system properties that might depend
+		// on others like
+		// log4j.appender.SQL.File=@@loggingBasePath@@/@@port@@.sql
+		// loggingBasePath=/var/log/@@name@@
+		// name and port are resolved via WOApplication.application()
+		// ERXLogger.configureLoggingWithSystemProperties();
+
+		ERXProperties.pathsForUserAndBundleProperties(true);
+	}
+
+	/**
+	 * Initializes your WOApplication programmatically (for use in test cases
+	 * and main methods) with the assumption that the current directory is your
+	 * main bundle URL.
+	 * 
+	 * @param applicationSubclass
+	 *            your Application subclass
+	 * @param args
+	 *            the commandline arguments for your application
+	 */
+	public static void initApp(Class applicationSubclass, String[] args) {
+		try {
+			File woaFolder = new File(".").getCanonicalFile();
+			if (!woaFolder.getName().endsWith(".woa")) {
+				if (new File(woaFolder, ".project").exists()) {
+					File buildFolder = new File(new File(woaFolder, "build"), woaFolder.getName() + ".woa");
+					if (buildFolder.exists()) {
+						woaFolder = buildFolder;
+					}
+					else {
+						File distFolder = new File(new File(woaFolder, "dist"), woaFolder.getName() + ".woa");
+						if (distFolder.exists()) {
+							woaFolder = distFolder;
+						}
+						else {
+							// Bundle-less builds. Yay!
+							// throw new IllegalArgumentException("You must run
+							// your application from a .woa folder to call this
+							// method.");
+						}
+					}
+				}
+			}
+			ERXExtensions.initApp(null, woaFolder.toURI().toURL(), applicationSubclass, args);
 		}
 		catch (IOException e) {
 			throw new NSForwardException(e);
 		}
-    	_appInitialized = true;
-    }
+	}
+
+	/**
+	 * Initializes your WOApplication programmatically (for use in test cases
+	 * and main methods).
+	 * 
+	 * @param mainBundleName
+	 *            the name of your main bundle
+	 * @param applicationSubclass
+	 *            your Application subclass
+	 * @param args
+	 *            the commandline arguments for your application
+	 */
+	public static void initApp(String mainBundleName, Class applicationSubclass, String[] args) {
+		ERXExtensions.initApp(mainBundleName, null, applicationSubclass, args);
+	}
+
+	private static boolean _appInitialized = false;
+
+	/**
+	 * Initializes your WOApplication programmatically (for use in test cases
+	 * and main methods).
+	 * 
+	 * @param mainBundleName
+	 *            the name of your main bundle (or null to use mainBundleURL)
+	 * @param mainBundleURL
+	 *            the URL to your main bundle (ignored if mainBundleName is set)
+	 * @param applicationSubclass
+	 *            your Application subclass
+	 * @param args
+	 *            the commandline arguments for your application
+	 */
+	public static void initApp(String mainBundleName, URL mainBundleURL, Class applicationSubclass, String[] args) {
+		if (_appInitialized) {
+			return;
+		}
+		try {
+			ERXApplication.setup(args);
+			if (mainBundleURL != null) {
+				System.setProperty("webobjects.user.dir", new File(mainBundleURL.getFile()).getCanonicalPath());
+			}
+			// Odds are you are only using this method for test cases and
+			// development mode
+			System.setProperty("er.extensions.ERXApplication.developmentMode", "true");
+			ERXApplication.primeApplication(mainBundleName, mainBundleURL, applicationSubclass.getName());
+			// NSNotificationCenter.defaultCenter().postNotification(new
+			// NSNotification(ERXApplication.ApplicationDidCreateNotification,
+			// WOApplication.application()));
+		}
+		catch (IOException e) {
+			throw new NSForwardException(e);
+		}
+		_appInitialized = true;
+	}
 }
