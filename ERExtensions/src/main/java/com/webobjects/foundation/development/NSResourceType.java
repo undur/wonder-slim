e496c900f3e971617f160330ed6255d73925e998
Hugi Thordarson <hugi@karlmenn.is>
2021-05-20 13:38:08 +0000

    Revert "Integrate ERFoundation and ERWebObjects"
    This reverts commit 75deced68b559041182f145b22b27fa3e4cd575b.

--- ERExtensions/src/main/java/com/webobjects/foundation/development/NSResourceType.java
+++
@@ -1,5 +0,0 @@
-package com.webobjects.foundation.development;
-
-public enum NSResourceType {
-	Component, Model, D2WModel, Strings, WebServer, JavaClient, JavaClientResources, Java, InfoPlist, Other;
-}
