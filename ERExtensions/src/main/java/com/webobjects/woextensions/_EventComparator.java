edaacfe564725f1b29383654d139de216cbb1d1f
Hugi Thordarson <hugi@karlmenn.is>
2021-05-20 08:43:00 +0000

    Deleted japanese javadocs

--- ERExtensions/src/main/java/com/webobjects/woextensions/_EventComparator.java
+++ ERExtensions/src/main/java/com/webobjects/woextensions/_EventComparator.java
@@ -12,28 +12,22 @@ import com.webobjects.eocontrol.EOSortOrdering;
 import com.webobjects.foundation.NSComparator;
 import com.webobjects.foundation.NSSelector;
 
-/**
- * <span class="ja">
- *  このクラスは二つの EOEvent を比較する為に使用します。
- *  woextensions の private クラス
- * </span>
- */
 public class _EventComparator extends NSComparator {
-    protected boolean _compareAscending;
-    protected WOEventDisplayPage _controller;
+	protected boolean _compareAscending;
+	protected WOEventDisplayPage _controller;
 
-    public _EventComparator(NSSelector comparator, WOEventDisplayPage ctrl) {
-        super();
-        _compareAscending = (comparator == EOSortOrdering.CompareAscending) ;
-        _controller = ctrl;
-    }
+	public _EventComparator(NSSelector comparator, WOEventDisplayPage ctrl) {
+		super();
+		_compareAscending = (comparator == EOSortOrdering.CompareAscending);
+		_controller = ctrl;
+	}
 
-    @Override
-    public int compare(Object e1, Object e2) throws NSComparator.ComparisonException {
-        if (!(e1 instanceof EOEvent) || !(e2 instanceof EOEvent) || (e1 == null) || (e2 == null))
-            throw new NSComparator.ComparisonException("<"+getClass().getName()+" Unable to compare EOEvents. Either one of the arguments is not a EOEvent or is null. Comparison was made with " + e1 + " and " + e2 + "." );
+	@Override
+	public int compare(Object e1, Object e2) throws NSComparator.ComparisonException {
+		if (!(e1 instanceof EOEvent) || !(e2 instanceof EOEvent) || (e1 == null) || (e2 == null))
+			throw new NSComparator.ComparisonException("<" + getClass().getName() + " Unable to compare EOEvents. Either one of the arguments is not a EOEvent or is null. Comparison was made with " + e1 + " and " + e2 + ".");
 
-        int result = ((EOEvent)e1)._compareDuration((EOEvent)e2);
-        return _compareAscending ? result : 0 - result;  
-    }
+		int result = ((EOEvent) e1)._compareDuration((EOEvent) e2);
+		return _compareAscending ? result : 0 - result;
+	}
 }
