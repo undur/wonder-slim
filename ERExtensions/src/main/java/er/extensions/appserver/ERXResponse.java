6845893813d0f332507cd3189609466b50ff21f3
Hugi Thordarson <hugi@karlmenn.is>
2021-05-22 07:37:38 +0000

    Deleted the unused ERXResponse.userInfo() stuff

--- ERExtensions/src/main/java/er/extensions/appserver/ERXResponse.java
+++ ERExtensions/src/main/java/er/extensions/appserver/ERXResponse.java
@@ -1,20 +1,8 @@
 package er.extensions.appserver;
 
-import java.util.Objects;
-
 import com.webobjects.appserver.WOContext;
 import com.webobjects.appserver.WOResponse;
-import com.webobjects.foundation.NSMutableDictionary;
 
-/**
- * ERXResponse provides a place to override methods of WOResponse. This is
- * returned by default from ERXApplication. Also has support for "partials",
- * i.e. in your render tree, you can define a new "partial", where the content
- * will actually get rendered.
- * 
- * @author mschrag
- * @author ak
- */
 public class ERXResponse extends WOResponse {
 
 	private WOContext _context;
@@ -32,40 +20,5 @@ public class ERXResponse extends WOResponse {
 
 	public ERXResponse(WOContext context) {
 		_context = context;
-	}
-
-	/**
-	 * WO 5.4 API Sets the value for key in the user info dictionary.
-	 * 
-	 * @param value
-	 *            value to add to userInfo()
-	 * @param key
-	 *            key to add value under
-	 */
-	@Override
-	public void setUserInfoForKey(Object value, String key) {
-		Objects.requireNonNull(value);
-		Objects.requireNonNull(key);
-		
-		NSMutableDictionary newUserInfo = new NSMutableDictionary(value, key);
-
-		if (userInfo() != null) {
-			newUserInfo.addEntriesFromDictionary(userInfo());
-		}
-
-		setUserInfo(newUserInfo);
-	}
-
-	/**
-	 * WO 5.4 API
-	 * 
-	 * @param key
-	 *            key to return value from userInfo() for
-	 * @return value from {@link #userInfo()} for key, or <code>null</code> if not available
-	 */
-	@Override
-	public Object userInfoForKey(String key) {
-		Objects.requireNonNull(key);
-		return userInfo() != null ? userInfo().objectForKey(key) : null;
 	}
 }
