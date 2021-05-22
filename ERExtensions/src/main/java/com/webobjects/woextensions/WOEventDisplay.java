95d530c411a97da1b9d924a2f0ea0a15ee3e0d20
Hugi Thordarson <hugi@karlmenn.is>
2021-05-22 06:19:07 +0000

    Use Wonder's type safe pageWithName

--- ERExtensions/src/main/java/com/webobjects/woextensions/WOEventDisplay.java
+++ ERExtensions/src/main/java/com/webobjects/woextensions/WOEventDisplay.java
@@ -1,5 +1,4 @@
 /*
- * WOEventDisplay.java
  * (c) Copyright 2001 Apple Computer, Inc. All rights reserved.
  * This a modified version.
  * Original license: http://www.opensource.apple.com/apsl/
@@ -8,16 +7,18 @@
 package com.webobjects.woextensions;
 
 import com.webobjects.appserver.WOActionResults;
-import com.webobjects.appserver.WODirectAction;
 import com.webobjects.appserver.WORequest;
 
-public class WOEventDisplay extends WODirectAction {
-    public WOEventDisplay(WORequest aRequest)  {
-        super(aRequest);
-    }
+import er.extensions.appserver.ERXDirectAction;
 
-    @Override
-    public WOActionResults defaultAction() {
-        return pageWithName("WOEventDisplayPage");
-    }
+public class WOEventDisplay extends ERXDirectAction {
+
+	public WOEventDisplay(WORequest aRequest) {
+		super(aRequest);
+	}
+
+	@Override
+	public WOActionResults defaultAction() {
+		return pageWithName(WOEventDisplayPage.class);
+	}
 }
