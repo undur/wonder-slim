a585904b1f397315db1a6d60ba9ed24ca67e6a17
hugi <hugi@192.168.1.97>
2021-06-19 10:42:00 +0000

    Removed unused import

--- ERExtensions/src/main/java/x/ERXDeprecatedConstant.java
+++ ERExtensions/src/main/java/x/ERXDeprecatedConstant.java
@@ -1,12 +1,9 @@
 package x;
 
-import com.webobjects.foundation.NSNotification;
-
 public class ERXDeprecatedConstant {
 
 	public static final Class[] NotificationClassArray = { com.webobjects.foundation.NSNotification.class };
 	public static final Class[] StringClassArray = new Class[] { String.class };
 	public static final Object[] EmptyObjectArray = new Object[] {};
 	public static final Class[] EmptyClassArray = new Class[0];
-
 }
