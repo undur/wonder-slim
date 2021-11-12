fb51268e0849028fa92d960e752055bc4affa856
Hugi Thordarson <hugi@karlmenn.is>
2021-11-12 10:43:16 +0000

    Deleted ERXStringUtilities.lastPropertyKeyInKeyPath()

--- ERExtensions/src/main/java/er/extensions/foundation/ERXStringUtilities.java
+++ ERExtensions/src/main/java/er/extensions/foundation/ERXStringUtilities.java
@@ -192,19 +192,4 @@ public class ERXStringUtilities {
 	 */
 	public static String safeIdentifierName(String source) {
 		return safeIdentifierName(source, "_", '_');
-	}
-
-	// FIXME: Re-added since JavaMonitor turned out to be still using it.
-	@Deprecated
-	public static final String lastPropertyKeyInKeyPath(String keyPath) {
-		String part = null;
-		if (keyPath != null) {
-			int index = keyPath.lastIndexOf(NSKeyValueCodingAdditions.KeyPathSeparator);
-			if (index != -1)
-				part = keyPath.substring(index + 1);
-			else
-				part = keyPath;
-		}
-		return part;
-	}
-}
+	}}
