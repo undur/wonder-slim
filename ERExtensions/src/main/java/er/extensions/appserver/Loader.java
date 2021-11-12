f06a2b1ac50455a75e6202e8d57a3d0db3a043a9
Hugi Thordarson <hugi@karlmenn.is>
2021-11-12 15:51:53 +0000

    Make JarChecker a static inner class of Loader

--- ERExtensions/src/main/java/er/extensions/appserver/Loader.java
+++ ERExtensions/src/main/java/er/extensions/appserver/Loader.java
@@ -13,6 +13,7 @@ import java.net.URL;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -37,6 +38,9 @@ import com.webobjects.foundation.NSBundle;
 import com.webobjects.foundation.NSDictionary;
 import com.webobjects.foundation.NSForwardException;
 import com.webobjects.foundation.NSKeyValueCoding;
+import com.webobjects.foundation.NSMutableArray;
+import com.webobjects.foundation.NSMutableDictionary;
+import com.webobjects.foundation.NSMutableSet;
 import com.webobjects.foundation.NSNotification;
 import com.webobjects.foundation.NSNotificationCenter;
 import com.webobjects.foundation.NSProperties;
@@ -569,6 +573,127 @@ public class Loader {
 			Object key = keys.objectAtIndex(i);
 			Object value = propertiesFromArgv.objectForKey(key);
 			NSProperties._setProperty((String) key, (String) value);
+		}
+	}
+	
+	/**
+	 * Utility class to track down duplicate items in the class path. Reports
+	 * duplicate packages and packages that are present in different versions.
+	 * 
+	 * @author ak
+	 */
+	public static class JarChecker {
+		private static final Logger startupLog = Logger.getLogger("er.extensions.ERXApplication.Startup");
+
+		private static class Entry {
+			long _size;
+			String _jar;
+
+			public Entry(long aL, String jar) {
+				_size = aL;
+				_jar = jar;
+			}
+
+			public long size() {
+				return _size;
+			}
+
+			public String jar() {
+				return _jar;
+			}
+
+			@Override
+			public boolean equals(Object other) {
+				if (other != null && other instanceof Entry) {
+					return ((Entry) other).size() == size();
+				}
+				return false;
+			}
+
+			@Override
+			public int hashCode() {
+				return (int) _size;
+			}
+
+			@Override
+			public String toString() {
+				return size() + "->" + jar();
+			}
+		}
+
+		private NSMutableDictionary<String, NSMutableArray<String>> packages = new NSMutableDictionary<>();
+
+		private NSMutableDictionary<String, NSMutableSet<Entry>> classes = new NSMutableDictionary<>();
+
+		void processJar(String jar) {
+			File jarFile = new File(jar);
+			if (!jarFile.exists() || jarFile.isDirectory()) {
+				return;
+			}
+			try( JarFile f = new JarFile(jar)) {
+				for (Enumeration<JarEntry> enumerator = f.entries(); enumerator.hasMoreElements();) {
+					JarEntry entry = enumerator.nextElement();
+					String name = entry.getName();
+					if (entry.getName().endsWith("/") && !(name.matches("^\\w+/$") || name.startsWith("META-INF"))) {
+						NSMutableArray<String> bundles = packages.objectForKey(name);
+						if (bundles == null) {
+							bundles = new NSMutableArray<>();
+							packages.setObjectForKey(bundles, name);
+						}
+						bundles.addObject(jar);
+					}
+					else if (!(name.startsWith("src") || name.startsWith("META-INF"))) {
+						Entry e = new Entry(entry.getSize(), jar);
+						NSMutableSet<Entry> set = classes.objectForKey(name);
+						if (set == null) {
+							set = new NSMutableSet<>();
+							classes.setObjectForKey(set, name);
+						}
+						set.addObject(e);
+					}
+				}
+			}
+			catch (IOException e) {
+				startupLog.error("Error in processing jar: " + jar, e);
+			}
+		}
+
+		void reportErrors() {
+			StringBuilder sb = new StringBuilder();
+			String message = null;
+
+			NSMutableArray<String> keys = new NSMutableArray<>( packages.allKeys() );
+			Collections.sort(keys);
+
+			for (Enumeration<String> enumerator = keys.objectEnumerator(); enumerator.hasMoreElements();) {
+				String packageName = enumerator.nextElement();
+				NSMutableArray<String> bundles = packages.objectForKey(packageName);
+				if (bundles.count() > 1) {
+					sb.append('\t').append(packageName).append("->").append(bundles).append('\n');
+				}
+			}
+			message = sb.toString();
+			if (message.length() > 0) {
+				startupLog.debug("The following packages appear multiple times:\n" + message);
+			}
+			sb = new StringBuilder();
+			NSMutableSet<String> classPackages = new NSMutableSet<>();
+			keys = new NSMutableArray<>( classes.allKeys());
+			Collections.sort(keys);
+			
+			for (Enumeration<String> enumerator = keys.objectEnumerator(); enumerator.hasMoreElements();) {
+				String className = enumerator.nextElement();
+				String packageName = className.replaceAll("/[^/]+?$", "");
+				NSMutableSet<Entry> bundles = classes.objectForKey(className);
+				if (bundles.count() > 1 && !classPackages.containsObject(packageName)) {
+					sb.append('\t').append(packageName).append("->").append(bundles).append('\n');
+					classPackages.addObject(packageName);
+				}
+			}
+			message = sb.toString();
+			if (message.length() > 0) {
+				startupLog.debug("The following packages have different versions, you should remove the version you don't want:\n" + message);
+			}
 		}
 	}
 }
