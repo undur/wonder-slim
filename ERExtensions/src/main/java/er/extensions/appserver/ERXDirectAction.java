4bda2ed108dfb08822e3e00841ae6956844f3dca
Hugi Thordarson <hugi@karlmenn.is>
2021-12-25 16:24:53 +0000

    Added a new ERXDirectAction class

---
+++ ERExtensions/src/main/java/er/extensions/appserver/ERXDirectAction.java
@@ -0,0 +1,14 @@
+package er.extensions.appserver;
+
+import com.webobjects.appserver.WORequest;
+
+/**
+ * Added as a placeholder for the actual DirectAction stuff 
+ */
+
+public class ERXDirectAction extends ERXAdminDirectAction {
+
+	public ERXDirectAction(WORequest r) {
+		super(r);
+	}
+}
