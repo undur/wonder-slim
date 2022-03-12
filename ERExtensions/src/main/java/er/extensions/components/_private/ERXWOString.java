af82a54782057fee9c538c3b9d8db18b02b385d5
Hugi Thordarson <hugi@karlmenn.is>
2022-03-12 13:50:51 +0000

    Add note on redunant local variable

--- ERExtensions/src/main/java/er/extensions/components/_private/ERXWOString.java
+++ ERExtensions/src/main/java/er/extensions/components/_private/ERXWOString.java
@@ -69,7 +69,7 @@ public class ERXWOString extends WODynamicElement {
 			valueInComponent = _value.valueInComponent(component);
 			if (_shouldFormat) {
 				Format format = null;
-				boolean hasFormatter = false;
+				boolean hasFormatter = false; // FIXME: This can probably be deleted (remnants of the ERXSession.autoAdjustTimeZones() thing)
 				if (_formatter != null) {
 					format = (Format) _formatter.valueInComponent(component);
 				}
