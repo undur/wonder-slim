064410ae72cf285cba07af870ef828cbe2657592
Hugi Thordarson <hugi@karlmenn.is>
2022-03-13 09:06:08 +0000

    Some final touches and comments on ERXMessageEncoding

--- ERExtensions/src/main/java/er/extensions/appserver/ERXMessageEncoding.java
+++ ERExtensions/src/main/java/er/extensions/appserver/ERXMessageEncoding.java
@@ -5,6 +5,13 @@ import java.nio.charset.Charset;
 import com.webobjects.appserver.WORequest;
 import com.webobjects.appserver.WOResponse;
 
+/**
+ * Encoding utility methods.
+ * 
+ * FIXME Hugi 2022-03-13: We're currently maintaining a singleton and other ceremony that's not required for the currrent functionality, and probably not required at all.
+ * It's purely to allow the older mechanism from Project Wonder to be re-implemented (which is probably not needed anymore)
+ */
+
 public class ERXMessageEncoding {
 
 	private static String _defaultEncoding = "utf-8";
@@ -28,25 +35,16 @@ public class ERXMessageEncoding {
 		return Charset.forName(defaultEncoding());
 	}
 
-	private static void setEncodingToResponse(WOResponse response, String encoding) {
-
+	public void setEncodingToResponse(WOResponse response) {
 		final String mimeType = response.headerForKey("Content-Type");
 
 		if (mimeType != null && (mimeType.equals("text/html") || mimeType.equals("text/xml"))) {
-			response.setContentEncoding(encoding);
+			response.setContentEncoding(defaultEncoding());
 			response.setHeader(mimeType + "; charset=" + defaultEncoding(), "Content-Type");
 		}
 	}
 
-	public void setEncodingToResponse(WOResponse response) {
-		setEncodingToResponse(response, defaultEncoding());
-	}
-
-	private static void setDefaultFormValueEncodingToRequest(WORequest request, String encoding) {
-		request.setDefaultFormValueEncoding(encoding);
-	}
-
 	public void setDefaultFormValueEncodingToRequest(WORequest request) {
-		setDefaultFormValueEncodingToRequest(request, defaultEncoding());
+		request.setDefaultFormValueEncoding(defaultEncoding());
 	}
 }
