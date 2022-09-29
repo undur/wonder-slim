7b6f8ceb62e05c3726d17f137ec119158a3d7247
Hugi Thordarson <hugi@karlmenn.is>
2022-09-29 06:31:42 +0000

    Syntax cleanup

--- ERLoggingReload4j/src/main/java/er/extensions/logging/ERXTemporaryLoggingBridge.java
+++ ERLoggingReload4j/src/main/java/er/extensions/logging/ERXTemporaryLoggingBridge.java
@@ -4,11 +4,10 @@ import java.util.Enumeration;
 
 import org.apache.log4j.Appender;
 import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Logger;
 
 /**
  * This class exists only as a temporary bridge while we work out receiving instructions from ERExtensions in a nicer manner
- * 
- *  FIXME: Hugi // 2022-03-15 
  */
 
 public class ERXTemporaryLoggingBridge {
@@ -21,7 +20,7 @@ public class ERXTemporaryLoggingBridge {
 	 * ak: telling Log4J to re-init the Console appenders so we get logging into WOOutputPath again
 	 */
 	public static void reInitConsoleAppenders() {
-		for( Enumeration<Appender> e = org.apache.log4j.Logger.getRootLogger().getAllAppenders(); e.hasMoreElements(); ) {
+		for( Enumeration<Appender> e = Logger.getRootLogger().getAllAppenders(); e.hasMoreElements(); ) {
 			final Appender appender = e.nextElement();
 
 			if( appender instanceof ConsoleAppender app ) {
