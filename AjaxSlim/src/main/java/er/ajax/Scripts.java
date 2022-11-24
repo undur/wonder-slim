8dfa168dc785fb3629b4717968f78fc1f72dec65
Hugi Thordarson <hugi@karlmenn.is>
2022-11-24 07:53:04 +0000

    Adding more script constants

--- AjaxSlim/src/main/java/er/ajax/Scripts.java
+++ AjaxSlim/src/main/java/er/ajax/Scripts.java
@@ -2,7 +2,11 @@ package er.ajax;
 
 public class Scripts {
 
+	// Moved
 	public static final String WONDER_JS = "wonder.js";
 	public static final String PROTOTYPE_JS = "prototype.js";
 	public static final String SCRIPTACULOUS_JS = "scriptaculous.js";
+	
+	// Not moved
+	public static final String EFFECTS_JS = "scriptaculous.js";
 }
