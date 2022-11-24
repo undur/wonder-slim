3a4284551ef416dbe488040b4389e83228663e94
Hugi Thordarson <hugi@karlmenn.is>
2022-11-24 21:44:35 +0000

    Syntax cleanup

--- AjaxSlim/src/main/java/er/ajax/Scripts.java
+++ AjaxSlim/src/main/java/er/ajax/Scripts.java
@@ -2,11 +2,8 @@ package er.ajax;
 
 public class Scripts {
 
-	// Moved
 	public static final String WONDER_JS = "wonder.js";
 	public static final String PROTOTYPE_JS = "prototype.js";
 	public static final String SCRIPTACULOUS_JS = "scriptaculous.js";
-	
-	// Not moved
 	public static final String EFFECTS_JS = "effects.js";
 }
