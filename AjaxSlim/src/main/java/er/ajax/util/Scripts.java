b05588004c044718597c152da3373f4a1b947bb0
Hugi Thordarson <hugi@karlmenn.is>
2022-11-24 07:36:46 +0000

    Add some constants for string the script filenames

---
+++ AjaxSlim/src/main/java/er/ajax/util/Scripts.java
@@ -0,0 +1,8 @@
+package er.ajax.util;
+
+public class Scripts {
+
+	public static final String WONDER_JS = "wonder.js";
+	public static final String PROTOTYPE_JS = "wonder.js";
+	public static final String SCRIPTACULOUS_JS = "wonder.js";
+}
