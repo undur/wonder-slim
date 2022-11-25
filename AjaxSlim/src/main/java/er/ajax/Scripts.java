e46a976326fd2ca90745576c8ce37cd089a7d448
Hugi Thordarson <hugi@karlmenn.is>
2022-11-25 07:44:31 +0000

    Don't use the new scripts

--- AjaxSlim/src/main/java/er/ajax/Scripts.java
+++ AjaxSlim/src/main/java/er/ajax/Scripts.java
@@ -11,23 +11,23 @@ public class Scripts {
 	private static final String EFFECTS_JS = "effects.js";
 
 	public static String wonderJS() {
-		return useNew ? WONDER_SLIM_JS : WONDER_JS;
+		return useNewScripts ? WONDER_SLIM_JS : WONDER_JS;
 	}
 
 	public static String prototypeJS() {
-		return useNew ? EMPTY_JS : PROTOTYPE_JS;
+		return useNewScripts ? EMPTY_JS : PROTOTYPE_JS;
 	}
 
 	public static String scriptaculousJS() {
-		return useNew ? EMPTY_JS : SCRIPTACULOUS_JS;
+		return useNewScripts ? EMPTY_JS : SCRIPTACULOUS_JS;
 	}
 
 	public static String effectsJS() {
-		return useNew ? EMPTY_JS : EFFECTS_JS;
+		return useNewScripts ? EMPTY_JS : EFFECTS_JS;
 	}
 
 	/**
 	 * Indicates whether we want to use the new script files
 	 */
-	public static boolean useNew = true;
+	public static boolean useNewScripts = false;
 }
