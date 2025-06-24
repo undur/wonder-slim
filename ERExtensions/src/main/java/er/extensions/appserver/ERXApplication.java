b574923e0d721ab0dde20f89748df77445d80c15
Hugi Thordarson <hugi@karlmenn.is>
2025-06-24 13:40:30 +0000

    Don't enable the KVC hack by default. Yet.

--- ERExtensions/src/main/java/er/extensions/appserver/ERXApplication.java
+++ ERExtensions/src/main/java/er/extensions/appserver/ERXApplication.java
@@ -53,7 +53,6 @@ import com.webobjects.foundation.NSTimestamp;
 
 import er.extensions.ERXExtensions;
 import er.extensions.ERXFrameworkPrincipal;
-import er.extensions.ERXKVCReflectionHack;
 import er.extensions.ERXLoggingSupport;
 import er.extensions.ERXMonitorServer;
 import er.extensions.appserver.ajax.ERXAjaxApplication;
@@ -171,7 +170,6 @@ public abstract class ERXApplication extends ERXAjaxApplication {
 		wasERXApplicationMainInvoked = true;
 		useProjectBundleIfDeveloping();
 		disablePBXProjectWatcher();
-		ERXKVCReflectionHack.enable();
 		setup(argv);
 		WOApplication.main(argv, applicationClass);
 	}
