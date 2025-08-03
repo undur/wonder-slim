ac391f0ba7fbfb95d53c6447262eae017f9637f7
Hugi Thordarson <hugi@karlmenn.is>
2025-08-03 11:13:43 +0000

    Apply syntax formatting to ERXComponentRequestHandler

--- ERExtensions/src/main/java/er/extensions/appserver/ERXComponentRequestHandler.java
+++ ERExtensions/src/main/java/er/extensions/appserver/ERXComponentRequestHandler.java
@@ -1,6 +1,5 @@
 package er.extensions.appserver;
 
-
 import com.webobjects.appserver.WOActionResults;
 import com.webobjects.appserver.WOApplication;
 import com.webobjects.appserver.WOComponent;
@@ -42,11 +41,10 @@ public class ERXComponentRequestHandler extends WORequestHandler {
 		if (pathArray != null) {
 			count = pathArray.count();
 		}
-		if ((pathArray != null) && (count != 0))
-		{
-			lastObject = (String)pathArray.lastObject();
+		if ((pathArray != null) && (count != 0)) {
+			lastObject = (String) pathArray.lastObject();
 			if (count > 1) {
-				penultElement = (String)pathArray.objectAtIndex(count - 2);
+				penultElement = (String) pathArray.objectAtIndex(count - 2);
 			}
 
 			length = lastObject.length();
@@ -54,18 +52,16 @@ public class ERXComponentRequestHandler extends WORequestHandler {
 			while ((p < length) && (Character.isDigit(lastObject.charAt(p)))) {
 				p++;
 			}
-			if ((p < length) && (lastObject.charAt(p) == '.'))
-			{
+			if ((p < length) && (lastObject.charAt(p) == '.')) {
 				aContextID = lastObject.substring(0, p);
 
 				p++;
 				aSenderID = lastObject.substring(p);
 
-				if ((penultElement != null) && (penultElement.endsWith(".wo")))
-				{
+				if ((penultElement != null) && (penultElement.endsWith(".wo"))) {
 					pageNameLength = count - 2;
-				} else if (penultElement != null)
-				{
+				}
+				else if (penultElement != null) {
 					if ((!_lookForIDsInCookiesFirst) || (aSessionID == null)) {
 						aSessionID = penultElement;
 					}
@@ -86,16 +82,14 @@ public class ERXComponentRequestHandler extends WORequestHandler {
 				if (lastObject.endsWith(".wo")) {
 					pageNameLength = count;
 				}
-				else
-				{
+				else {
 					aSessionID = lastObject;
 
 					pageNameLength = count - 1;
 				}
 			}
-			
-			if ((aSessionID == null) && (!_lookForIDsInCookiesFirst))
-			{
+
+			if ((aSessionID == null) && (!_lookForIDsInCookiesFirst)) {
 				aSessionID = aRequest.stringFormValueForKey(WOApplication.application().sessionIdKey());
 				if (aSessionID == null) {
 					aSessionID = aRequest.cookieValueForKey(WOApplication.application().sessionIdKey());
@@ -103,8 +97,7 @@ public class ERXComponentRequestHandler extends WORequestHandler {
 			}
 
 		}
-		else if (WOApplication.application().shouldRestoreSessionOnCleanEntry(aRequest))
-		{
+		else if (WOApplication.application().shouldRestoreSessionOnCleanEntry(aRequest)) {
 			aSessionID = aRequest.cookieValueForKey(WOApplication.application().sessionIdKey());
 		}
 
@@ -114,8 +107,7 @@ public class ERXComponentRequestHandler extends WORequestHandler {
 		return aDictionary;
 	}
 
-	private WOComponent _restorePageForContextID(String oldContextID, WOSession aSession)
-	{
+	private WOComponent _restorePageForContextID(String oldContextID, WOSession aSession) {
 		return aSession.restorePageForContextID(oldContextID);
 	}
 
@@ -132,23 +124,19 @@ public class ERXComponentRequestHandler extends WORequestHandler {
 
 		aContext._setResponse(aResponse);
 
-		if (oldContextID == null)
-		{
-			if (aSenderID != null)
-			{
+		if (oldContextID == null) {
+			if (aSenderID != null) {
 				if (aRequest._hasFormValues()) {
 					anApplication.takeValuesFromRequest(aRequest, aContext);
 				}
 			}
 
 			aContext._setPageChanged(false);
-			if (aSenderID != null)
-			{
+			if (aSenderID != null) {
 				WOActionResults anActionResults = anApplication.invokeAction(aRequest, aContext);
 
-				if ((anActionResults == null) || ((anActionResults instanceof WOComponent)))
-				{
-					WOComponent aResultComponent = (WOComponent)anActionResults;
+				if ((anActionResults == null) || ((anActionResults instanceof WOComponent))) {
+					WOComponent aResultComponent = (WOComponent) anActionResults;
 					if ((aResultComponent != null) && (aResultComponent.context() != aContext)) {
 						aResultComponent._awakeInContext(aContext);
 					}
@@ -162,8 +150,7 @@ public class ERXComponentRequestHandler extends WORequestHandler {
 					}
 
 				}
-				else
-				{
+				else {
 					WOResponse theResponse = anActionResults.generateResponse();
 
 					return theResponse;
@@ -171,8 +158,7 @@ public class ERXComponentRequestHandler extends WORequestHandler {
 			}
 
 		}
-		else
-		{
+		else {
 			WOComponent responsePage = _restorePageForContextID(oldContextID, aSession);
 			aContext._setPageElement(responsePage);
 		}
@@ -185,16 +171,14 @@ public class ERXComponentRequestHandler extends WORequestHandler {
 	private WOResponse _dispatchWithPreparedSession(WOSession aSession, WOContext aContext, NSDictionary someElements) {
 		WOComponent aPage = null;
 		WOResponse aResponse = null;
-		String aPageName = (String)someElements.objectForKey("wopage");
+		String aPageName = (String) someElements.objectForKey("wopage");
 		String oldContextID = aContext._requestContextID();
-		String oldSessionID = (String)someElements.objectForKey(WOApplication.application().sessionIdKey());
+		String oldSessionID = (String) someElements.objectForKey(WOApplication.application().sessionIdKey());
 		WOApplication anApplication = WOApplication.application();
 		boolean clearIDsInCookies = false;
 
-		if ((oldSessionID == null) || (oldContextID == null))
-		{
-			if ((aPageName == null) && (!aSession.storesIDsInCookies()))
-			{
+		if ((oldSessionID == null) || (oldContextID == null)) {
+			if ((aPageName == null) && (!aSession.storesIDsInCookies())) {
 				WORequest request = aContext.request();
 
 				String cookieHeader = request.headerForKey("cookie");
@@ -210,8 +194,7 @@ public class ERXComponentRequestHandler extends WORequestHandler {
 
 			aPage = anApplication.pageWithName(aPageName, aContext);
 		}
-		else
-		{
+		else {
 			aPage = _restorePageForContextID(oldContextID, aSession);
 			if (aPage == null) {
 				if (anApplication._isPageRecreationEnabled())
@@ -239,29 +222,45 @@ public class ERXComponentRequestHandler extends WORequestHandler {
 		WOSession aSession = null;
 		WOResponse aResponse = null;
 		WOResponse errorResponse = null;
-		String aSessionID = (String)someElements.objectForKey(WOApplication.application().sessionIdKey());
+		String aSessionID = (String) someElements.objectForKey(WOApplication.application().sessionIdKey());
 
 		if (aSessionID == null) {
-			
-			// FIXME: OK, so I don't *think* the component request handler should ever perform session creation.
-			// When allowed to create a session, it really overrides the actual mechanism for handling a missing session AND the defaultRequestHandler mechanism, and just tries to construct a component with the name "Main" (potentially just resulting in a WOPageNotFoundException). 
-			// During regular use of this handler, a session should already exist. The only situation where I can see session creation as required is upon direct named page access (which this handler is not supposed to allow).
-			// So, instead of creating a session if one is missing, we just go directly to handling a session restoration error.
-			// This isn't really optimal because we didn't actually fail to "restore" a known session ID, there should really be separate error handling for a missing session ID when expected. But I guess this is close enough.
-			// This might fail if this request handler is your application's default request handler (haven't tried it). But that's something you'd explicitly have to do,
-			// and I don't know why you'd ever do that (until we decide for some reason that we need to completely replace WO's component request handler). 
+
+			// FIXME: OK, so I don't *think* the component request handler
+			// should ever perform session creation.
+			// When allowed to create a session, it really overrides the actual
+			// mechanism for handling a missing session AND the
+			// defaultRequestHandler mechanism, and just tries to construct a
+			// component with the name "Main" (potentially just resulting in a
+			// WOPageNotFoundException).
+			// During regular use of this handler, a session should already
+			// exist. The only situation where I can see session creation as
+			// required is upon direct named page access (which this handler is
+			// not supposed to allow).
+			// So, instead of creating a session if one is missing, we just go
+			// directly to handling a session restoration error.
+			// This isn't really optimal because we didn't actually fail to
+			// "restore" a known session ID, there should really be separate
+			// error handling for a missing session ID when expected. But I
+			// guess this is close enough.
+			// This might fail if this request handler is your application's
+			// default request handler (haven't tried it). But that's something
+			// you'd explicitly have to do,
+			// and I don't know why you'd ever do that (until we decide for some
+			// reason that we need to completely replace WO's component request
+			// handler).
 			// Hugi 2025-08-02
 
-			errorResponse = anApplication.handleSessionRestorationErrorInContext( aContext );
+			errorResponse = anApplication.handleSessionRestorationErrorInContext(aContext);
 
 			/*
-			 * Old logic, where this request handler was allowed to create a new session
+			 * Old logic, where this request handler was allowed to create a new
+			 * session
 			 *
-			aSession = anApplication._initializeSessionInContext( aContext );
-			if( aSession == null ) {
-				errorResponse = anApplication.handleSessionCreationErrorInContext( aContext );
-			}	
-			*/
+			 * aSession = anApplication._initializeSessionInContext( aContext );
+			 * if( aSession == null ) { errorResponse =
+			 * anApplication.handleSessionCreationErrorInContext( aContext ); }
+			 */
 		}
 		else {
 			aSession = anApplication.restoreSessionWithID(aSessionID, aContext);
@@ -270,31 +269,30 @@ public class ERXComponentRequestHandler extends WORequestHandler {
 			}
 		}
 
-		if (errorResponse == null)
-		{
+		if (errorResponse == null) {
 			aResponse = _dispatchWithPreparedSession(aSession, aContext, someElements);
 		}
-		else aResponse = errorResponse;
+		else
+			aResponse = errorResponse;
 
 		aContext._putAwakeComponentsToSleep();
 		anApplication.saveSessionForContext(aContext);
 		return aResponse;
 	}
 
-	WOResponse _handleRequest(WORequest aRequest)
-	{
+	WOResponse _handleRequest(WORequest aRequest) {
 		WOContext aContext = null;
 		WOResponse aResponse;
 
 		NSDictionary requestHandlerValues = requestHandlerValuesForRequest(aRequest);
 
 		WOApplication anApplication = WOApplication.application();
-		String aSessionID = (String)requestHandlerValues.objectForKey(WOApplication.application().sessionIdKey());
+		String aSessionID = (String) requestHandlerValues.objectForKey(WOApplication.application().sessionIdKey());
 
 		if ((!anApplication.isRefusingNewSessions()) || (aSessionID != null)) {
-			String aSenderID = (String)requestHandlerValues.objectForKey("woeid");
+			String aSenderID = (String) requestHandlerValues.objectForKey("woeid");
 
-			String oldContextID = (String)requestHandlerValues.objectForKey("wocid");
+			String oldContextID = (String) requestHandlerValues.objectForKey("wocid");
 
 			WOStatisticsStore aStatisticsStore = anApplication.statisticsStore();
 			if (aStatisticsStore != null)
@@ -311,10 +309,8 @@ public class ERXComponentRequestHandler extends WORequestHandler {
 
 				anApplication.sleep();
 			}
-			catch (Exception exception)
-			{
-				try
-				{
+			catch (Exception exception) {
+				try {
 					NSLog.err.appendln("<" + getClass().getName() + ">: Exception occurred while handling request:\n" + exception.toString());
 					if (NSLog.debugLoggingAllowedForLevelAndGroups(1, 4L)) {
 						NSLog.debug.appendln(exception);
@@ -327,21 +323,19 @@ public class ERXComponentRequestHandler extends WORequestHandler {
 					}
 					WOSession aSession = aContext._session();
 					aResponse = anApplication.handleException(exception, aContext);
-					if (aSession != null)
-					{
-						try
-						{
+					if (aSession != null) {
+						try {
 							anApplication.saveSessionForContext(aContext);
 							anApplication.sleep();
-						} catch (Exception eAgain) {
+						}
+						catch (Exception eAgain) {
 							NSLog.err.appendln("<WOApplication '" + anApplication.name() + "'>: Another Exception  occurred while trying to clean the application:\n" + eAgain.toString());
 							if (NSLog.debugLoggingAllowedForLevelAndGroups(1, 4L))
 								NSLog.debug.appendln(eAgain);
 						}
 					}
 				}
-				finally
-				{
+				finally {
 					if ((aContext != null) && (aContext._session() != null))
 						anApplication.saveSessionForContext(aContext);
 				}
@@ -371,17 +365,16 @@ public class ERXComponentRequestHandler extends WORequestHandler {
 	}
 
 	@Override
-	public WOResponse handleRequest(WORequest aRequest)
-	{
+	public WOResponse handleRequest(WORequest aRequest) {
 		WOApplication anApplication = WOApplication.application();
 		Object globalLock = anApplication.requestHandlingLock();
 		WOResponse aResponse;
-		if (globalLock != null)
-		{
+		if (globalLock != null) {
 			synchronized (globalLock) {
 				aResponse = _handleRequest(aRequest);
 			}
-		} else {
+		}
+		else {
 			aResponse = _handleRequest(aRequest);
 		}
 
