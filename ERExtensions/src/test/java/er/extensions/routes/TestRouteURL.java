400fa2ac3f8544efd161ce4b5cf04aea9ea016c3
Hugi Thordarson <hugi@karlmenn.is>
2025-10-11 17:28:55 +0000

    Add some tests for RouteURL

---
+++ ERExtensions/src/test/java/er/extensions/routes/TestRouteURL.java
@@ -0,0 +1,52 @@
+package er.extensions.routes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.Test;
+
+public class TestRouteURL {
+
+	@Test
+	public void removesSlashAtStartAndEnd() {
+		RouteURL url = RouteURL.create( "/url/" );
+		assertEquals( url.toString(), "url" );
+	}
+
+	@Test
+	public void length() {
+		RouteURL url = RouteURL.create( "/url/hugi/" );
+		assertEquals( url.length(), 2 );
+	}
+
+	@Test
+	public void integerValue() {
+		RouteURL url = RouteURL.create( "/url/2/haha" );
+		assertEquals( url.getInteger( 1 ), Integer.valueOf( 2 ) );
+	}
+
+	@Test
+	public void stringValue() {
+		RouteURL url = RouteURL.create( "/url/2/haha" );
+		assertEquals( url.getString( 0 ), "url" );
+		assertEquals( url.getString( 1 ), "2" );
+		assertEquals( url.getString( 2 ), "haha" );
+	}
+
+	@Test
+	public void stringValueExceedingLengthIsNull() {
+		RouteURL url = RouteURL.create( "/url/2/haha" );
+		assertNull( url.getString( 4 ) );
+	}
+
+	@Test
+	public void integerValueExceedingLengthIsNull() {
+		RouteURL url = RouteURL.create( "/url/2/haha" );
+		assertNull( url.getInteger( 4 ) );
+	}
+
+	@Test
+	public void howTheHeckToHandleDoubleSlashes() {
+		// FIXME: Implement. Empty path component or eliminate path component? // Hugi 2025-10-11
+	}
+}
