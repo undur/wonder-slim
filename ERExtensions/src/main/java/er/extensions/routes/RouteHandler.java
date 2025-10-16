82a0c3ba04f49f2e9a25bb67298b0a65f1d730ce
Hugi Thordarson <hugi@karlmenn.is>
2025-10-16 07:06:05 +0000

    Add note on RouteHandler

--- ERExtensions/src/main/java/er/extensions/routes/RouteHandler.java
+++ ERExtensions/src/main/java/er/extensions/routes/RouteHandler.java
@@ -6,6 +6,7 @@ import com.webobjects.appserver.WOContext;
 /**
  * FIXME: Really a duplicate of WORequestHandler (apart from returning WOActionResults instead of WOResponse, making it a bit more convenient) // Hugi 2025-10-11 
  * FIXME: Should be an interface // Hugi 2025-10-11
+ * FIXME: handle() should really accept something like a "RouteRequest" or "RouteContext" rather than the current parameters // Hugi 2025-10-16
  */
 
 public abstract class RouteHandler {
