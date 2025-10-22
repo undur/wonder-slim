e601289846d9cb7a738b2368b32b30d4c6e9b0ca
Hugi Thordarson <hugi@karlmenn.is>
2025-10-22 14:54:54 +0000

    Use plain java arrays for ERXRequest.HOST_ADDRESS_KEYS / .HOST_NAME_KEYS

--- ERExtensions/src/main/java/er/extensions/appserver/ERXRequest.java
+++ ERExtensions/src/main/java/er/extensions/appserver/ERXRequest.java
@@ -43,14 +43,18 @@ public  class ERXRequest extends WORequest {
     private static final String UNKNOWN_HOST = "UNKNOWN";
     private static final String X_FORWARDED_PROTO_FOR_SSL = ERXProperties.stringForKeyWithDefault("er.extensions.appserver.ERXRequest.xForwardedProtoForSsl", "https");
     private static final String X_FORWARDED_PROTO_HEADER_KEY_FOR_SSL = ERXProperties.stringForKeyWithDefault("er.extensions.appserver.ERXRequest.xForwardedProtoHeaderKeyForSsl", "x-forwarded-proto");
-    private static final NSArray<String> HOST_ADDRESS_KEYS = new NSArray<>(new String[]{"x-forwarded-for", "pc-remote-addr", "remote_host", "remote_addr", "remote_user", "x-webobjects-remote-addr"});
+    
+    /**
+     * Headers to check for the client IP-address 
+     */
+    private static final String[] HOST_ADDRESS_KEYS = {"x-forwarded-for", "pc-remote-addr", "remote_host", "remote_addr", "remote_user", "x-webobjects-remote-addr"};
 
     /**
      * 'Host' is the official HTTP 1.1 header for the host name in the request URL, so this should be checked first. @see http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.23
      * when the app is behind a reverse proxy 'Host' will contain the proxy address instead of the requested one so check first for 'x-forwarded-host' @see http://httpd.apache.org/docs/2.2/mod/mod_proxy.html#x-headers
      * Fallback headers such as server_name will screw up your complete URL generation for secure domains that have wildcard subdomains since it returns sth like *.domain.com for host name
      */
-    private static final NSArray<String> HOST_NAME_KEYS = new NSArray<>(new String[]{"x-forwarded-host", "Host", "x-webobjects-server-name", "server_name", "http_host"});
+    private static final String[] HOST_NAME_KEYS = {"x-forwarded-host", "Host", "x-webobjects-server-name", "server_name", "http_host"};
     
     /**
      * FIXME: Docs missing // Hugi 2025-10-22
