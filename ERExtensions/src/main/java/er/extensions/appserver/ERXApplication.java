36e3fea5d752092350e3580531525829d8962be6
Hugi Thordarson <hugi@karlmenn.is>
2025-10-31 08:48:36 +0000

    Moved URL rewriting to it's own class, ERXURLRewriteConfig

--- ERExtensions/src/main/java/er/extensions/appserver/ERXApplication.java
+++ ERExtensions/src/main/java/er/extensions/appserver/ERXApplication.java
@@ -100,14 +100,9 @@ public abstract class ERXApplication extends ERXAjaxApplication {
 	private Integer _sslPort;
 
 	/**
-	 * The path rewriting pattern to match (@see _rewriteURL)
+	 * Configuration for URL rewriting
 	 */
-	private String _replaceApplicationPathPattern;
-	
-	/**
-	 * The path rewriting replacement to apply to the matched pattern (@see _rewriteURL)
-	 */
-	private String _replaceApplicationPathReplace;
+	private final ERXURLRewriteConfig _urlRewriteConfig;
 
 	/**
 	 * To support load balancing with mod_proxy
@@ -212,7 +207,7 @@ public abstract class ERXApplication extends ERXAjaxApplication {
 		// Configure the WOStatistics CLFF logging since it can't be controlled by a property, grrr.
 		configureStatisticsLogging();
 
-		configureReplaceApplicationPath();
+		_urlRewriteConfig = new ERXURLRewriteConfig(this);
 
 		_publicHost = ERXProperties.stringForKeyWithDefault("er.extensions.ERXApplication.publicHost", host());
 
@@ -471,23 +466,58 @@ public abstract class ERXApplication extends ERXAjaxApplication {
 	}
 
 	/**
-	 * Invoked from the constructor to initialize the replaceApplicationPath
+	 * Holds configuration for an application's URL rewriting
+	 * 
+	 * @param replaceApplicationPathPattern The path rewriting pattern to match (@see _rewriteURL)
+	 * @param replaceApplicationPathReplace The path rewriting replacement to apply to the matched pattern (@see _rewriteURL)
 	 */
-	private void configureReplaceApplicationPath() {
-		_replaceApplicationPathPattern = ERXProperties.stringForKey("er.extensions.ERXApplication.replaceApplicationPath.pattern");
+	public record ERXURLRewriteConfig( String replaceApplicationPathPattern, String replaceApplicationPathReplace ) {
+		
+		public ERXURLRewriteConfig( ERXApplication app ) {
+			String _replaceApplicationPathPattern = ERXProperties.stringForKey("er.extensions.ERXApplication.replaceApplicationPath.pattern");
 
-		if (_replaceApplicationPathPattern != null && _replaceApplicationPathPattern.length() == 0) {
-			_replaceApplicationPathPattern = null;
-		}
-
-		_replaceApplicationPathReplace = ERXProperties.stringForKey("er.extensions.ERXApplication.replaceApplicationPath.replace");
-
-		if (_replaceApplicationPathPattern == null && rewriteDirectConnectURL()) {
-			_replaceApplicationPathPattern = "/cgi-bin/WebObjects/" + name() + applicationExtension();
-
-			if (_replaceApplicationPathReplace == null) {
-				_replaceApplicationPathReplace = "";
+			if (_replaceApplicationPathPattern != null && _replaceApplicationPathPattern.length() == 0) {
+				_replaceApplicationPathPattern = null;
 			}
+
+			String _replaceApplicationPathReplace = ERXProperties.stringForKey("er.extensions.ERXApplication.replaceApplicationPath.replace");
+
+			if (_replaceApplicationPathPattern == null && app.rewriteDirectConnectURL()) {
+				_replaceApplicationPathPattern = "/cgi-bin/WebObjects/" + app.name() + app.applicationExtension();
+
+				if (_replaceApplicationPathReplace == null) {
+					_replaceApplicationPathReplace = "";
+				}
+			}
+			
+			this( _replaceApplicationPathPattern, _replaceApplicationPathReplace );
+		}
+		
+		/**
+		 * Hook to rewrite generated URLs. Invoked by ERXWOContext.
+		 * 
+		 * You can also set "er.extensions.replaceApplicationPath.pattern" to the pattern
+		 * to match and "er.extensions.replaceApplicationPath.replace" to the value to replace it with.
+		 * 
+		 * For example, in Properties: <code>
+		 * er.extensions.ERXApplication.replaceApplicationPath.pattern=/cgi-bin/WebObjects/YourApp.woa
+		 * er.extensions.ERXApplication.replaceApplicationPath.replace=/yourapp
+		 * </code>
+		 * 
+		 * and in Apache 2.2: <code>
+		 * RewriteRule ^/yourapp(.*)$ /cgi-bin/WebObjects/YourApp.woa$1 [PT,L]
+		 * </code>
+		 * 
+		 * @param url the URL to rewrite
+		 * @return the rewritten URL
+		 */
+		public String rewriteURL(final String url) {
+
+			if (url != null && replaceApplicationPathPattern != null && replaceApplicationPathReplace != null) {
+				return url.replaceFirst(replaceApplicationPathPattern, replaceApplicationPathReplace);
+			}
+
+			return url;
 		}
 	}
 
@@ -868,30 +898,10 @@ public abstract class ERXApplication extends ERXAjaxApplication {
 	}
 
 	/**
-	 * Hook to rewrite generated URLs. Invoked by ERXWOContext.
-	 * 
-	 * You can also set "er.extensions.replaceApplicationPath.pattern" to the pattern
-	 * to match and "er.extensions.replaceApplicationPath.replace" to the value to replace it with.
-	 * 
-	 * For example, in Properties: <code>
-	 * er.extensions.ERXApplication.replaceApplicationPath.pattern=/cgi-bin/WebObjects/YourApp.woa
-	 * er.extensions.ERXApplication.replaceApplicationPath.replace=/yourapp
-	 * </code>
-	 * 
-	 * and in Apache 2.2: <code>
-	 * RewriteRule ^/yourapp(.*)$ /cgi-bin/WebObjects/YourApp.woa$1 [PT,L]
-	 * </code>
-	 * 
-	 * @param url the URL to rewrite
-	 * @return the rewritten URL
+	 * @return Rewritten URL
 	 */
 	public String _rewriteURL(final String url) {
-
-		if (url != null && _replaceApplicationPathPattern != null && _replaceApplicationPathReplace != null) {
-			return url.replaceFirst(_replaceApplicationPathPattern, _replaceApplicationPathReplace);
-		}
-
-		return url;
+		return _urlRewriteConfig.rewriteURL(url);
 	}
 
 	/**
