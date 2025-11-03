76fb97fbac833417f4bed170b97b0beb805d62ee
Hugi Thordarson <hugi@karlmenn.is>
2025-11-03 08:32:45 +0000

    Comments cleanup in app-based WebServerResource management

--- ERExtensions/src/main/java/er/extensions/resources/ERXAppBasedResourceManager.java
+++ ERExtensions/src/main/java/er/extensions/resources/ERXAppBasedResourceManager.java
@@ -7,25 +7,19 @@ import com.webobjects.foundation.NSArray;
 import er.extensions.appserver.ERXWOContext;
 
 /**
- * ResourceManager/RequestHandler implementations. For serving web server resources through the application (rather than the web server "split install")
+ * ResourceManager implementation for serving web server resources through the application (rather than the web server "split install")
  * 
  * URLs look the same in development and production, containing frameworkName, resourceName and languages.
  * The request handler looks at the URL, finds the data and serves it.
  * This means serving of resources is entirely controlled from within the application and works identically in development and production.
- *
- * Work to do before labeling this "totally ready":
- * 
- * FIXME: Resource cache needs work (currently stores all resources in-memory indefinitely in production) // Hugi 2025-10-04
- * TODO: Add some nice way to control client-side caching (i.e. set caching headers on the response) // Hugi 2025-10-04
- * TODO: Handle localized resources // Hugi 2025-10-04
- * TODO: ERXResourceManager's "resource versioning" is a nice idea, we could do with something like that // Hugi 2025-10-05
- * TODO: Look into "resource processing". E.g. for templating in resources // Hugi 2025-10-05   
  */
 
 public class ERXAppBasedResourceManager extends ERXResourceManagerBase {
 
 	/**
 	 * Generates a URL for the given resource. Format: .../App.woa/res/[framework]/[resourceName]?languages=[lang1,lang2,lang3]
+	 * 
+	 * FIXME: Handle localized resources // Hugi 2025-10-04
 	 */
 	@Override
 	public String urlForResourceNamed(String resourceName, String frameworkName, NSArray<String> languages, WORequest request) {
