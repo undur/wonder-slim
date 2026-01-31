48dab2fa2af92c09f3af90099d03e1314e070323
Hugi Thordarson <hugi@karlmenn.is>
2026-01-31 12:53:06 +0000

    Remove unused import

--- ERExtensions/src/main/java/er/extensions/appserver/ERXRequest.java
+++ ERExtensions/src/main/java/er/extensions/appserver/ERXRequest.java
@@ -3,7 +3,6 @@ package er.extensions.appserver;
 import java.net.HttpCookie;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Enumeration;
 import java.util.Map;
 
 import org.slf4j.Logger;
