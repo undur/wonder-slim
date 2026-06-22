4222754413130ea518deffaa54d1594b994e92f9
Hugi Thordarson <hugi@karlmenn.is>
2026-06-22 11:10:56 +0000

    Don't cache HTML responses (for now)

--- AjaxPlayground/src/main/java/ajaxplayground/Application.java
+++ AjaxPlayground/src/main/java/ajaxplayground/Application.java
@@ -3,6 +3,9 @@ package ajaxplayground;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.webobjects.appserver.WORequest;
+import com.webobjects.appserver.WOResponse;
+
 import er.extensions.appserver.ERXApplication;
 
 /**
@@ -30,5 +33,20 @@ public class Application extends ERXApplication {
 
 		// Clean, flat URLs for page-to-page navigation (see Routes).
 		Routes.register();
+	}
+	
+	@Override
+	public WOResponse dispatchRequest( WORequest request ) {
+		final WOResponse response = super.dispatchRequest( request );
+		
+		// CHECKME: We're temporarily disabling client-side caching on HTML pages
+		// Worksaround for something that should really be handled by AjaxSlim. See: https://github.com/undur/wonder-slim/issues/27
+		final String contentType = response.headerForKey( "content-type" );
+
+		if( contentType != null && contentType.startsWith( "text/html" ) ) {
+			response.disableClientCaching();
+		}
+
+		return response;
 	}
 }
