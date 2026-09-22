cd12298ed66f278137f2e63a6077b9d56681cbd4
Hugi Thordarson <hugi@karlmenn.is>
2026-09-22 08:42:00 +0000

    Mark the describe action as temporary

--- ERExtensions/src/main/java/er/extensions/appserver/ERXDirectAction.java
+++ ERExtensions/src/main/java/er/extensions/appserver/ERXDirectAction.java
@@ -21,6 +21,9 @@ public class ERXDirectAction extends ERXAdminDirectAction {
 	 * (see wonder-slim-deployment#56). Reachable on any wonder-slim application: as /wa/describe when the
 	 * application's default DirectAction extends this class (inherited actions dispatch), and as
 	 * /wa/ERXDirectAction/describe by classname routing regardless. Deliberately minimal: framework name only.
+	 *
+	 * FIXME: Temporary. Goes once ng-objects and wonder-slim share a common response structure for describing an
+	 * application to the deployment stack, which this then implements // Hugi 2026-09-22
 	 */
 	public WOActionResults describeAction() {
 		final WOResponse response = new WOResponse();
